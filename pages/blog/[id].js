3dfff16396c032d2d6f207b3b684cd0a59a2c568
森裕輔 <yusukew3@gmail.com>
2022-03-18 11:55:29 +0900

    category 追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -6,6 +6,7 @@ export default function BlogId({ blog }) {
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
