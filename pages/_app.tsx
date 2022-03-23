d61ba50a04be2514506623c5a128c8ef44ce6fdf
森裕輔 <yusukew3@gmail.com>
2022-03-23 10:37:53 +0900

    Footer ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../src/styles/globals.css'
 // import { ChakraProvider } from "@chakra-ui/react"
 import { Layout } from '../src/components/Layout'
 import 'ress'
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
