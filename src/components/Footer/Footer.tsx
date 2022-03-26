f64622ef24de69c71622acfb9fec691f63e3b7b3
森裕輔 <yusukew3@gmail.com>
2022-03-26 16:55:08 +0900

    nav sns Btn

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,49 @@
+import styles from "../../styles/Footer.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faDribbbleSquare } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
-    <div>
-      <p>Footerコンポーネント</p>
+    <div className={styles.footerWrap}>
+      <footer className={styles.footer}>
+        <ul className={styles.snsBtn}>
+          <li>
+            <a href="/">
+              <FontAwesomeIcon
+                icon={faTwitterSquare}
+                style={{ fontSize: 24, color: "#4A4A4A" }}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <FontAwesomeIcon
+                icon={faFacebookSquare}
+                style={{ fontSize: 24, color: "#4A4A4A" }}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <FontAwesomeIcon
+                icon={faGithubSquare}
+                style={{ fontSize: 24, color: "#4A4A4A" }}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <FontAwesomeIcon
+                icon={faDribbbleSquare}
+                style={{ fontSize: 24, color: "#4A4A4A" }}
+              />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
