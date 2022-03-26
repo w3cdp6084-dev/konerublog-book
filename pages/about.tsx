6c32b40e4d770a586cd5dc12bd70fd0f5405a530
森裕輔 <yusukew3@gmail.com>
2022-03-26 20:54:03 +0900

    about.tsx styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,25 @@
-export default function About() {
-    return <h1>About Page</h1>;
+import styles from '../src/styles/about.module.scss';
+
+  export default function About() {
+    return (
+      <main className={styles.mainWrap}>
+        <div className={styles.logoWrap}>
+          <img src="/image/logo.svg" alt="about logo" />
+        </div>
+        <div>
+          <h2>KONEKONE</h2>
+          <h3>Yusuke Mori <br />
+          Designer/Developer</h3>
+        </div>
+        <div>
+          <p>KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
+          ブランディングとコンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
+          デザイン・プログラムでいろいろな実験を個人で行ってますので、こちらはそのnoteにもなっています。
+          </p>
+        </div>
+        <div>
+          <p>portfolio</p>
+        </div>
+      </main>
+    );
   }
