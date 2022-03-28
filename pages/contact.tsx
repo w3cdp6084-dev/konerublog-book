5409a425d3b013d4bd37c71e5b35ca429b721226
森裕輔 <yusukew3@gmail.com>
2022-03-28 10:48:44 +0900

    contact.tsx styling

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import styles from '../src/styles/contact.module.scss';
+
+
+export default function About() {
+    return (
+      <main className={styles.mainWrap}>
+        <div className={styles.contentWrap}>
+
+        </div>
+      </main>
+    );
+  }
