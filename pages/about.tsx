80e07e7b2e207c43b27ffd513dc79de48b0ff08c
森裕輔 <yusukew3@gmail.com>
2022-03-28 10:29:28 +0900

    about.tsx styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,20 +9,22 @@ import styles from '../src/styles/about.module.scss';
               <img src="/image/logo.svg" alt="about logo" />
             </div>
           </div>
-          <div>
-            <h2 className="font-bold">KONEKONE</h2>
-            <h3>Yusuke Mori <br />
-            Designer/Developer</h3>
+          <div className='text-center'>
+            <h2 className="font-bold">KONEKONE.</h2>
+            <h3 className='mt-4'>Yusuke Mori.<br />
+            Designer./Developer.</h3>
           </div>
-          <div>
-            <p>KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
+          <div className='text-center mt-12 mb-20'>
+            <p className='leading-relaxed'>KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
             ブランディングとコンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
             デザイン・プログラムでいろいろな実験を個人で行ってますので、こちらはそのnoteにもなっています。
             </p>
           </div>
-          <div>
-            <p>portfolio</p>
-          </div>
+          <a href="https://konekone.org/">
+            <div className={styles.btnWrap}>
+                <p className='text-sm'>portfolio</p>
+            </div>
+          </a>
         </div>
       </main>
     );
