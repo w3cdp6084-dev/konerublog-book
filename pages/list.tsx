2d2d124ed1b120ffa8739353c613f5ea8400e8f2
森裕輔 <yusukew3@gmail.com>
2022-04-10 21:17:09 +0900

    list tsx

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,6 +1,34 @@
+import Link from "next/link";
+import { client } from "../libs/client";
 
-  export default function About() {
+  export default function List({blog}) {
     return (
-     <div>list</div>
+      <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
     );
   }
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  const key = {
+    headers: {'X-MICROCMS-API-KEY': process.env.API_KEY},
+  };
+  const datas = await fetch('https://konerublog-book.microcms.io/api/v1/blog?offset=0&limit=5', key)
+  .then(res => res.json())
+  .catch(() => null);
+  return {
+    props: {
+      blog: data.contents,
+      totalCount: datas.totalCount
+    },
+  };
+};
